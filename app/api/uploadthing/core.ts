3ad65c8438a46bbda2c6a8393f18be28ebc20e18
patryk <paniczgg@gmail.com>
2025-01-15 16:06:56 +0100

    added banner

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -7,19 +7,32 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({
-    image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
-      maxFileSize: "4MB",
-      maxFileCount: 10,
-    },
-  })
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 10 } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
+      const { getUser } = getKindeServerSession();
+      const user = await getUser();
+
+      // If you throw, the user will not be able to upload
+      if (!user || user.email !== "paniczgg@gmail.com")
+        throw new UploadThingError("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+
+  bannerImageRoute: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
       const { getUser } = getKindeServerSession();
       const user = await getUser();
 
---
+++ app/dashboard/banner/page.tsx
@@ -0,0 +1,106 @@
+import prisma from "@/app/lib/db";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { MoreHorizontal, PlusCircle, User2 } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export async function getData() {
+  const data = await prisma.banner.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+}
+
+const BannerRoute = async () => {
+  const data = await getData();
+  return (
+    <>
+      <div className="flex items-center justify-end">
+        <Button asChild className="flex gap-x-2">
+          <Link href="/dashboard/banner/create">
+            <PlusCircle className="size-3.5" />
+            <span>Create Banner</span>
+          </Link>
+        </Button>
+      </div>
+      <Card className="mt-5">
+        <CardHeader>
+          <CardTitle>Banners</CardTitle>
+          <CardDescription>Manage your banner</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Image</TableHead>
+                <TableHead>Title</TableHead>
+                <TableHead className="text-end">Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {data.map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell>
+                    <Image
+                      src={item.imageString}
+                      alt="Banner image"
+                      width={64}
+                      height={64}
+                      className="rounded-lg object-cover size-16"
+                    />
+                  </TableCell>
+                  <TableCell className="font-medium">{item.title}</TableCell>
+                  <TableCell className="text-end">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button size="icon" variant="ghost">
+                          <MoreHorizontal className="size-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem asChild>
+                          <Link href={`/dashboard/banner/${item.id}/delete`}>Delete</Link>
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default BannerRoute;
